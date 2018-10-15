dfff24ac86267e752b1f425cff4068cc87225821
Lior Haim <lior@daynix.com>
2018-10-15 14:05:37 +0300

    Reading CLI arguments
    Signed-off-by: Lior Haim <lior@daynix.com>

--- dropbox_uploader.rb
+++ dropbox_uploader.rb
@@ -1,4 +1,4 @@
-require './lib/dropbox_api'
+require 'json'
 
 # Reading credentials
 CONFIG_JSON = 'config.json'.freeze
@@ -7,3 +7,8 @@ config = JSON.parse(File.read(CONFIG_JSON))
 GITHUB_LOGIN = config['github_credentials']['login']
 GITHUB_PASSWORD = config['github_credentials']['password']
 DROPBOX_TOKEN = config['dropbox_token']
+
+# Reading CLI args
+repo = ARGV[0]
+commit = ARGV[1]
+path = ARGV[2]
